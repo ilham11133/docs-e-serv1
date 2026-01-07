e09e68dbb883d52d1cc04cfc2c5c7be298bde1fc
Ilhammao <bintangsahputra93@gmail.com>
2026-01-07 20:23:53 +0700

    changes

--- src/components/HomepageFeatures/index.js
+++ src/components/HomepageFeatures/index.js
@@ -1,64 +1,35 @@
 import React from 'react';
-import clsx from 'clsx';
-import styles from './styles.module.css';
+import Layout from '@theme/Layout';
 
-const FeatureList = [
-  {
-    title: 'Easy to Use',
-    Svg: require('@site/static/img/undraw_docusaurus_mountain.svg').default,
-    description: (
-      <>
-        Docusaurus was designed from the ground up to be easily installed and
-        used to get your website up and running quickly.
-      </>
-    ),
-  },
-  {
-    title: 'Focus on What Matters',
-    Svg: require('@site/static/img/undraw_docusaurus_tree.svg').default,
-    description: (
-      <>
-        Docusaurus lets you focus on your docs, and we&apos;ll do the chores. Go
-        ahead and move your docs into the <code>docs</code> directory.
-      </>
-    ),
-  },
-  {
-    title: 'Powered by React',
-    Svg: require('@site/static/img/undraw_docusaurus_react.svg').default,
-    description: (
-      <>
-        Extend or customize your website layout by reusing React. Docusaurus can
-        be extended while reusing the same header and footer.
-      </>
-    ),
-  },
-];
-
-function Feature({Svg, title, description}) {
+export default function Home() {
   return (
-    <div className={clsx('col col--4')}>
-      <div className="text--center">
-        <Svg className={styles.featureSvg} role="img" />
-      </div>
-      <div className="text--center padding-horiz--md">
-        <h3>{title}</h3>
-        <p>{description}</p>
-      </div>
-    </div>
-  );
-}
-
-export default function HomepageFeatures() {
-  return (
-    <section className={styles.features}>
-      <div className="container">
-        <div className="row">
-          {FeatureList.map((props, idx) => (
-            <Feature key={idx} {...props} />
-          ))}
-        </div>
-      </div>
-    </section>
+    <Layout title="Documentation">
+      <main style={{
+        backgroundColor: '#000000', // Polos Hitam
+        height: '90vh', // Menyesuaikan tinggi layar (dikurangi navbar jika ada)
+        display: 'flex',
+        flexDirection: 'column',
+        justifyContent: 'center',
+        alignItems: 'center',
+        color: 'white'
+      }}>
+        {/* Logo Kamu - Pastikan path gambarnya benar di folder static/img/ */}
+        <img 
+          src="img/logo.png" 
+          alt="Logo" 
+          style={{ width: '150px', marginBottom: '20px' }} 
+        />
+        
+        {/* Teks Documentation */}
+        <h1 style={{ 
+          fontSize: '3rem', 
+          fontWeight: 'bold', 
+          letterSpacing: '5px',
+          textTransform: 'uppercase' 
+        }}>
+          DOCUMENTATION
+        </h1>
+      </main>
+    </Layout>
   );
 }
